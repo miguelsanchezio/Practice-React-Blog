248bfdebc2393fff04ff190eab683365c44ad606
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-18 20:38:46 -0700

    Added react-router-dom, initial routes, and initial links.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from '../Blog/NewPost/NewPost';
 
 class Blog extends Component {
 	render () {
@@ -10,12 +12,13 @@ class Blog extends Component {
 				<header>
 					<nav>
 						<ul>
-							<li><a href="/">Home</a></li>
-							<li><a href="/new-post">New Post</a></li>			
+							<li><Link to="/">Home</Link></li>
+							<li><Link to="/new-post">New Post</Link></li>			
 						</ul>
 					</nav>
 				</header>
-				<Posts/>
+				<Route path="/" exact component={Posts}/>
+				<Route path="/new-post" exact component={NewPost}/>				
 			</div>
 		);
 	}
