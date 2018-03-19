01e27b8fd41a1c0e8c1cefe400e9377350454aa6
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-19 11:40:20 -0700

    Fullpost now extracs the route parameters.

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,10 +8,10 @@ class FullPost extends Component {
 		loadedPost: null
 	}
 
-	componentDidUpdate() {
-		if(this.props.id) {
-			if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-				axios.get('/posts/' + this.props.id)
+	componentDidMount() {
+		if(this.props.match.params.id) {
+			if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)) {
+				axios.get('/posts/' + this.props.match.params.id)
 				.then(res => {
 					this.setState({ loadedPost: res.data });
 				});
@@ -20,7 +20,7 @@ class FullPost extends Component {
 	}
 
 	deletePostHandler = () => {
-		axios.delete('/posts/' + this.props.id)
+		axios.delete('/posts/' + this.props.match.params.id)
 			.then(res => {
 				console.log(res);
 			});
@@ -28,7 +28,7 @@ class FullPost extends Component {
 
 	render () {
 		let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-		if(this.props.id) {
+		if(this.props.match.params.id) {
 			post = <p style={{textAlign: 'center'}}>Loading post...</p>;
 		}
 		if(this.state.loadedPost) {
